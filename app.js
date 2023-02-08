ed709cd3ee4e15c9f9db5dcff412e48b7a46c839
whokeshav <94471056+whokeshav@users.noreply.github.com>
2023-02-08 22:35:34 +0530

    last updates to the website

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 var btntranslate=document.querySelector("#btn-translate")
 var txtinput=document.querySelector("#txt-input")
-console.log(txtinput)
+var outputdiv=document.querySelector("#output")
+var serverurl="https://api.funtranslations.com/translate/minion.json"
+
+ function getTranslationurl(text)
+ {
+    return serverurl + "?" + "text=" + text 
+ }
+ function errorhandeler(error)
+ {
+    console.log("error occured",error)
+    alert("something wrong with server . try again after sometime ") 
+ }
 function clickHandler()
-{console.log("clicked")}
-if (btntranslate) {
-    btntranslate.addEventListener("click",clickHandler())
+{
+    var inputtext = txtinput.value;
+
+    fetch(getTranslationurl(inputtext))
+     .then(response=>response.json())
+     .then(json=>
+        {
+        var translatedtext = json.contents.translated;
+        outputdiv.innerText = translatedtext;
+                  
+        }
+
+         )
+    .catch(errorhandeler)
 }
+btntranslate.addEventListener("click",clickHandler )
 
 
- 
+  
